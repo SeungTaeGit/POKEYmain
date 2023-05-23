5a1c3b16c0ee0c7e6affeed9e507e1928ff31448
SeungTaeGit <129585999+SeungTaeGit@users.noreply.github.com>
2023-05-23 23:13:25 +0900

    Add files via upload

---
+++ POKEYHOME/src/main/java/capstone/pokey/POKEYHOME/PokeyhomeApplication.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYHOME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeyhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeyhomeApplication.class, args);
+	}
+
+}
---
+++ POKEYHOME/src/test/java/capstone/pokey/POKEYHOME/PokeyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYHOME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
